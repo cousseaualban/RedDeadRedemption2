16391752be3474a5c5489df8697f0c095fccaa45
acousseau <acousseau@projetlineaire.com>
2023-07-17 09:38:18 +0200

    Projet Final RDR2 version 5

--- resources/views/front/accueil.blade.php
+++ resources/views/front/accueil.blade.php
@@ -112,30 +112,32 @@
 
     <!-- Début section footer -->
     <footer class="footer">
-        <img src="/vendor/assets/logo.png" alt="Logo Red Dead Redemeption 2">
-        <div class="coordonnees">
+        <div class="nav">
+            <img src="/vendor/assets/logo.png" alt="Logo Red Dead Redemeption 2">
+            <div class="coordonnees">
 
-            <div class="uneCoordonnee">
-                <i class="fa fa-map-pin"></i>
-                <p>85000, La Roche sur Yon</p>
+                <div class="uneCoordonnee">
+                    <i class="fa fa-map-pin"></i>
+                    <p>85000, La Roche sur Yon</p>
+                </div>
+
+                <div class="uneCoordonnee">
+                    <i class="fa fa-envelope"></i>
+                    <p>cousseaualban@gmail.com</p>
+                </div>
+
+                <div class="uneCoordonnee">
+                    <i class="fa fa-phone"></i>
+                    <p>02 44 23 82 15 </p>
+                </div>
+
             </div>
 
-            <div class="uneCoordonnee">
-                <i class="fa fa-envelope"></i>
-                <p>cousseaualban@gmail.com</p>
+            <div class="liensAnnexes">
+                <p><a href="#">Mentions Légales</a></p>
+                <p><a href="#">Politique de confidentialié</a></p>
+                <p><a href="#">Contact</a></p>
             </div>
-
-            <div class="uneCoordonnee">
-                <i class="fa fa-phone"></i>
-                <p>02 44 23 82 15 </p>
-            </div>
-
-        </div>
-
-        <div class="liensAnnexes">
-            <p><a href="#">Mentions Légales</a></p>
-            <p><a href="#">Politique de confidentialié</a></p>
-            <p><a href="#">Contact</a></p>
         </div>
         <hr>
         <div class="reseauxSociaux">
